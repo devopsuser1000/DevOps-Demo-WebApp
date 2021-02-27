a20cce9cc60a0920f73bf0a8a02a2a7f7b282760
Garvit Mehrotra <garvitmehrotra@Garvits-MacBook-Air.local>
2021-02-27 15:02:02 -0500

    change IP addresses

--- src/test/java/servlet/createpage.java
+++ src/test/java/servlet/createpage.java
@@ -17,7 +17,7 @@ public class createpage extends HttpServlet {
 		try
 		{
 			Class.forName("org.postgresql.Driver");
-			String jdbcUrl="jdbc:postgresql://3.129.92.148:5432/postgres";
+			String jdbcUrl="jdbc:postgresql://3.17.207.180:5432/postgres";
 			String username="postgres";
 			String password="password";
 		
